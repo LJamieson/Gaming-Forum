b3ff2f495ada18f819e312e23a41df71f31e8bab
Lewis Jamieson <36374188+LJamieson@users.noreply.github.com>
2018-11-04 15:09:45 +0000

    Update CWsite.py

--- sourcecode/CWsite.py
+++ sourcecode/CWsite.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
-app. secret_key = ' supersecret
+app. secret_key = 'supersecret'
 
 @app.route('/')
 def home():
